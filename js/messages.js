e5fec631e80f61d1a50a48fa35ce7847264d65de
Лидия <lida_demina06@mail.ru>
2025-12-28 19:19:31 +0500

    12.13. Подготовиться к защите проекта

--- js/messages.js
+++ js/messages.js
@@ -4,24 +4,33 @@ const showMessage = (templateId, buttonClass) => {
   const template = document.querySelector(templateId).content.cloneNode(true);
   const message = template.querySelector('section');
 
+  message.style.position = 'fixed';
+  message.style.top = '50%';
+  message.style.left = '50%';
+  message.style.transform = 'translate(-50%, -50%)';
+  message.style.zIndex = '1000';
+  message.style.display = 'flex';
+  message.style.justifyContent = 'center';
+  message.style.alignItems = 'center';
+  message.style.minWidth = '300px';
+  message.style.minHeight = '100px';
+  message.style.textAlign = 'center';
+
   document.body.append(message);
+
+  const onEsc = (evt) => {
+    if (isEscapeKey(evt)) {
+      remove();
+    }
+  };
 
   function remove () {
     message.remove();
     document.removeEventListener('keydown', onEsc);
   }
 
-  function onEsc (evt) {
-    if (isEscapeKey(evt)) {
-      remove();
-    }
-  }
-
   message.addEventListener('click', (evt) => {
-    if (
-      evt.target.classList.contains(buttonClass) ||
-      evt.target === message
-    ) {
+    if (evt.target.classList.contains(buttonClass) || evt.target === message) {
       remove();
     }
   });
