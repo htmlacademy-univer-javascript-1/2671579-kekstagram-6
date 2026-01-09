3000ee36a2ff2b64539cd9795cb3b9c5251295fe
Лидия <lida_demina06@mail.ru>
2026-01-09 23:24:16 +0500

    Испарвила код

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,77 +1,82 @@
-import { isEscapeKey } from './util.js';
+import {isEscapeKey} from './util.js';
 
-const COMMENT_PER_PORTION = 5;
+const COMMENTS_PER_PORTION = 5;
 
 const bigPictureElement = document.querySelector('.big-picture');
 const bigPictureImgElement = bigPictureElement.querySelector('.big-picture__img img');
-const bigPictureLikesElement = bigPictureElement.querySelector('.likes-count');
-const bigPictureCommentsCountElement = bigPictureElement.querySelector('.comments-count');
-const bigPictureDescriptionElement = bigPictureElement.querySelector('.social__caption');
+const likesCountElement = bigPictureElement.querySelector('.likes-count');
+const descriptionElement = bigPictureElement.querySelector('.social__caption');
 
+const commentsCountBlock = bigPictureElement.querySelector('.social__comment-count');
 const commentsListElement = bigPictureElement.querySelector('.social__comments');
+const commentsLoaderElement = bigPictureElement.querySelector('.comments-loader');
+const closeButtonElement = bigPictureElement.querySelector('.big-picture__cancel');
+
+commentsCountBlock.innerHTML = '';
+
+const shownCountElement = document.createElement('span');
+shownCountElement.classList.add('social__comment-shown-count');
+
+const totalCountElement = document.createElement('span');
+totalCountElement.classList.add('social__comment-total-count');
+
+commentsCountBlock.append(
+  shownCountElement,
+  document.createTextNode(' из '),
+  totalCountElement,
+  document.createTextNode(' комментариев')
+);
+
 const commentTemplate = commentsListElement.querySelector('.social__comment');
-
-const commentCountBlock = bigPictureElement.querySelector('.social__comment-count');
-const commentsLoader = bigPictureElement.querySelector('.comments-loader');
-
-const closeButton = bigPictureElement.querySelector('.big-picture__cancel');
 
 let currentComments = [];
 let shownCommentsCount = 0;
-
-let shownCountSpan = commentCountBlock.querySelector('.social__comment-shown-count');
-let totalCountSpan = commentCountBlock.querySelector('.social__comment-total-count');
-
-if (!shownCountSpan) {
-  shownCountSpan = document.createElement('span');
-  shownCountSpan.classList.add('social__comment-shown-count');
-  commentCountBlock.prepend(shownCountSpan, document.createTextNode(' из '));
-}
-
-if (!totalCountSpan) {
-  totalCountSpan = document.createElement('span');
-  totalCountSpan.classList.add('social__comment-total-count');
-  commentCountBlock.append(totalCountSpan, document.createTextNode(' комментариев'));
-}
 
 const clearComments = () => {
   commentsListElement.innerHTML = '';
 };
 
-const renderCommentsPortion = () => {
-  const fragment = document.createDocumentFragment();
-  const nextPortion = currentComments.slice(shownCommentsCount, shownCommentsCount + COMMENT_PER_PORTION);
+const updateCommentsCounter = () => {
+  shownCountElement.textContent = shownCommentsCount;
+  totalCountElement.textContent = currentComments.length;
+};
 
-  nextPortion.forEach(({ avatar, name, message }) => {
+const renderComments = () => {
+  const fragment = document.createDocumentFragment();
+
+  const commentsToRender = currentComments.slice(
+    shownCommentsCount,
+    shownCommentsCount + COMMENTS_PER_PORTION
+  );
+
+  commentsToRender.forEach(({ avatar, name, message }) => {
     const commentElement = commentTemplate.cloneNode(true);
 
-    const img = commentElement.querySelector('.social__picture');
-    const text = commentElement.querySelector('.social__text');
+    const avatarElement = commentElement.querySelector('.social__picture');
+    const textElement = commentElement.querySelector('.social__text');
 
-    img.src = avatar;
-    img.alt = name;
-    text.textContent = message;
+    avatarElement.src = avatar;
+    avatarElement.alt = name;
+    textElement.textContent = message;
 
     fragment.appendChild(commentElement);
   });
 
   commentsListElement.appendChild(fragment);
 
-  shownCommentsCount += nextPortion.length;
-
-  shownCountSpan.textContent = shownCommentsCount;
-  totalCountSpan.textContent = currentComments.length;
+  shownCommentsCount += commentsToRender.length;
+  updateCommentsCounter();
 
   if (shownCommentsCount >= currentComments.length) {
-    commentsLoader.classList.add('hidden');
+    commentsLoaderElement.classList.add('hidden');
   } else {
-    commentsLoader.classList.remove('hidden');
+    commentsLoaderElement.classList.remove('hidden');
   }
 };
 
-commentsLoader.addEventListener('click', () => {
-  renderCommentsPortion();
-});
+const onCommentsLoaderClick = () => {
+  renderComments();
+};
 
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -80,22 +85,18 @@ const onDocumentKeydown = (evt) => {
   }
 };
 
-const renderBigPicture = ({ url, description, likes, comments }) => {
+const openBigPicture = ({ url, description, likes, comments }) => {
   bigPictureImgElement.src = url;
   bigPictureImgElement.alt = description;
 
-  bigPictureLikesElement.textContent = likes;
-  bigPictureCommentsCountElement.textContent = comments.length;
-  bigPictureDescriptionElement.textContent = description;
+  likesCountElement.textContent = likes;
+  descriptionElement.textContent = description;
 
   currentComments = comments;
   shownCommentsCount = 0;
+
   clearComments();
-
-  commentCountBlock.classList.remove('hidden');
-  commentsLoader.classList.remove('hidden');
-
-  renderCommentsPortion();
+  renderComments();
 
   bigPictureElement.classList.remove('hidden');
   document.body.classList.add('modal-open');
@@ -108,12 +109,10 @@ function closeBigPicture() {
   document.body.classList.remove('modal-open');
 
   clearComments();
-
   document.removeEventListener('keydown', onDocumentKeydown);
 }
 
-closeButton.addEventListener('click', () => {
-  closeBigPicture();
-});
+commentsLoaderElement.addEventListener('click', onCommentsLoaderClick);
+closeButtonElement.addEventListener('click', closeBigPicture);
 
-export {renderBigPicture};
+export {openBigPicture};
